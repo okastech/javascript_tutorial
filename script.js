ddd2493377e41daf46cda6c8860efa532c7d85c3
okastech <okastech@gmail.com>
2025-06-26 18:39:59 +0100

    third commit

--- script.js
+++ script.js
@@ -172,7 +172,6 @@ const rest2 = {
   owner: 'giovani Rossi',
 };
 
-console.log('hello world');
 //-----------OR ASSIGNMENT OPERATOR----------//
 // rest1.numGuest = rest1.numGuest || 10;
 // rest2.numGuest = rest2.numGuest || 10;
