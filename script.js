339e2d64d6f5941d04dba69a771f8f46663a9a28
okastech <okastech@gmail.com>
2025-06-26 18:13:08 +0100

    My second commit

--- script.js
+++ script.js
@@ -172,6 +172,7 @@ const rest2 = {
   owner: 'giovani Rossi',
 };
 
+console.log('hello world');
 //-----------OR ASSIGNMENT OPERATOR----------//
 // rest1.numGuest = rest1.numGuest || 10;
 // rest2.numGuest = rest2.numGuest || 10;
